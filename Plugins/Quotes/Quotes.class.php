4e429896d4ef14f6dff298e3ef0cae46f6d66d7c
Yohann Lorant <yohann.lorant@gmail.com>
2017-02-15 03:14:43 +0100

    Updates indents on last commit

--- Plugins/Quotes/Quotes.class.php
+++ Plugins/Quotes/Quotes.class.php
@@ -20,24 +20,24 @@ class Quotes extends PluginBase
      */
     public function CommandAddquote($command, $args)
     {
-		// Check rights
-		if(!$command['moderator'])
-			return;
-		
-		if(!count($args))
-			return IRC::reply($command, "Insufficient parameters.");
-		
-		array_shift($args); // Remove the command name from the args to get the actual quote
-		
-		// Create channel if needed
-		if(!isset($this->quotes[$command['channel']]))
-			$this->quotes[$command['channel']] = [];
-		
-		$this->quotes[$command['channel']][] = join(' ', $args);
-		$this->data->set('quotes', $this->quotes);
-		
-		$index = count($this->quotes[$command['channel']]); // Count = last index + 1, basically the quote index
-		IRC::reply($command, 'Quote #'. $index. ' added.');
+        // Check rights
+        if(!$command['moderator'])
+            return;
+        
+        if(!count($args))
+            return IRC::reply($command, "Insufficient parameters.");
+        
+        array_shift($args); // Remove the command name from the args to get the actual quote
+        
+        // Create channel if needed
+        if(!isset($this->quotes[$command['channel']]))
+            $this->quotes[$command['channel']] = [];
+        
+        $this->quotes[$command['channel']][] = join(' ', $args);
+        $this->data->set('quotes', $this->quotes);
+        
+        $index = count($this->quotes[$command['channel']]); // Count = last index + 1, basically the quote index
+        IRC::reply($command, 'Quote #'. $index. ' added.');
     }
     
     /**
@@ -45,27 +45,27 @@ class Quotes extends PluginBase
      */
     public function CommandEditquote($command, $args)
     {
-    	if(!$command['moderator'])
-    		return;
-    	
-    	if(count($args) < 2)
-    		return IRC::reply($command, "Insufficient parameters.");
-    	
-    	array_shift($args); // Remove command name
-    	$quoteID = array_shift($args) - 1; // Get quote ID
-    	
-		// Create channel if needed
-		if(!isset($this->quotes[$command['channel']]))
-			$this->quotes[$command['channel']] = [];
-		
-		if(!isset($this->quotes[$command['channel']][$quoteID]))
-			return IRC::reply($command, "This quote doesn't exist");
-		
-		// Update the quote
-		$this->quotes[$command['channel']][$quoteID] = join(' ', $args);
-		$this->data->set('quotes', $this->quotes);
-		
-		IRC::reply($command, "Quote #". $quoteID. " updated.");
+        if(!$command['moderator'])
+            return;
+        
+        if(count($args) < 2)
+            return IRC::reply($command, "Insufficient parameters.");
+        
+        array_shift($args); // Remove command name
+        $quoteID = array_shift($args) - 1; // Get quote ID
+        
+        // Create channel if needed
+        if(!isset($this->quotes[$command['channel']]))
+            $this->quotes[$command['channel']] = [];
+        
+        if(!isset($this->quotes[$command['channel']][$quoteID]))
+            return IRC::reply($command, "This quote doesn't exist");
+        
+        // Update the quote
+        $this->quotes[$command['channel']][$quoteID] = join(' ', $args);
+        $this->data->set('quotes', $this->quotes);
+        
+        IRC::reply($command, "Quote #". $quoteID. " updated.");
     }
 
 	/**
@@ -73,25 +73,25 @@ class Quotes extends PluginBase
 	 */
 	public function CommadnDelquote($command, $args)
 	{
-    	if(!$command['moderator'])
-    		return;
-    	
-    	if(!count($args))
-    		return IRC::reply($command, "Insufficient parameters.");
-    	
-    	$quoteID = array_shift($args) - 1; // Get quote ID
-    	
-		// Create channel if needed
-		if(!isset($this->quotes[$command['channel']]))
-			$this->quotes[$command['channel']] = [];
-		
-		if(!isset($this->quotes[$command['channel']][$quoteID]))
-			return IRC::reply($command, "This quote doesn't exist");
-		
-		unset($this->quotes[$command['channel']][$quoteID]);
-		$this->data->set('quotes', $this->quotes);
-		
-		IRC::reply($command, "Quote deleted.");
+        if(!$command['moderator'])
+            return;
+        
+        if(!count($args))
+            return IRC::reply($command, "Insufficient parameters.");
+        
+        $quoteID = array_shift($args) - 1; // Get quote ID
+        
+        // Create channel if needed
+        if(!isset($this->quotes[$command['channel']]))
+            $this->quotes[$command['channel']] = [];
+        
+        if(!isset($this->quotes[$command['channel']][$quoteID]))
+            return IRC::reply($command, "This quote doesn't exist");
+        
+        unset($this->quotes[$command['channel']][$quoteID]);
+        $this->data->set('quotes', $this->quotes);
+        
+        IRC::reply($command, "Quote deleted.");
 	}
 	
 	/**
@@ -99,20 +99,20 @@ class Quotes extends PluginBase
 	 */
 	public function CommandQuote($command, $args)
 	{
-		// If there isn't any quote for this channel, don't do anything
-		if(empty($this->quotes[$command['channel']]))
-			return;
-	
-		$quoteID = null;
-		
-		if(!empty($args[0]))
-			$quoteID = intval($args[0]) - 1;
-		else
-			$quoteID = rand(0, count($this->quotes[$command['channel']]) - 1);
-		
-		if(!isset($this->quotes[$command['channel']][$quoteID]))
-			return IRC::reply($command, "This quote doesn't exist.");
-		
-		IRC::reply($command, "[#". ($quoteID + 1). "] ". $this->quotes[$command['channel']][$quoteID]);
+        // If there isn't any quote for this channel, don't do anything
+        if(empty($this->quotes[$command['channel']]))
+            return;
+        
+        $quoteID = null;
+        
+        if(!empty($args[0]))
+            $quoteID = intval($args[0]) - 1;
+        else
+            $quoteID = rand(0, count($this->quotes[$command['channel']]) - 1);
+        
+        if(!isset($this->quotes[$command['channel']][$quoteID]))
+            return IRC::reply($command, "This quote doesn't exist.");
+        
+        IRC::reply($command, "[#". ($quoteID + 1). "] ". $this->quotes[$command['channel']][$quoteID]);
 	}
 }
