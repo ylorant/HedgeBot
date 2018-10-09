0a90bcc08fa1ea122cd98e576ba9a3e006993122
Yohann Lorant <yohann.lorant@gmail.com>
2018-10-09 02:25:10 +0200

    Updated Docblock in EventManager

--- Core/Events/EventManager.php
+++ Core/Events/EventManager.php
@@ -1,25 +1,12 @@
 <?php
 
 /**
- * \file core/Events.class.php
- * \author Yohann Lorant <yohann.lorant@gmail.com>
- * \version 0.5
- * \brief Events class file.
- *
- * \section LICENSE
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details at
- * http://www.gnu.org/copyleft/gpl.html
- *
- * \section DESCRIPTION
+ * @file core/Events.class.php
+ * @author Yohann Lorant <yohann.lorant@gmail.com>
+ * @version 0.5
+ * @brief Events class file.
+ * 
+ * @section DESCRIPTION
  *
  * This file hosts the Events class, handling all events.
  */
